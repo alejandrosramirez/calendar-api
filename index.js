dc996f88cc428f3f27067181e7381701ebced8e2
Alejandro Salgado <alejandrosram@outlook.com>
2022-08-05 00:01:38 -0500

    Update with routes

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hola");
+const express = require("express");
+require("dotenv").config();
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.use("/api/auth", require("./routes/auth"));
+
+app.listen(process.env.API_PORT, () => {
+	console.log(`Servidor corriendo en ${process.env.API_PORT}`)
+});
