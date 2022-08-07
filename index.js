60d982100c544a43585e782cd642ffdd1f5dbaf7
Alejandro Salgado <alejandrosram@outlook.com>
2022-08-07 14:50:53 -0500

    update port

--- index.js
+++ index.js
@@ -17,6 +17,6 @@ app.use(express.json());
 app.use("/api/auth", require("./routes/auth.route"));
 app.use("/api/events", require("./routes/event.route"));
 
-app.listen(process.env.API_PORT, () => {
-	console.log(`Servidor corriendo en ${process.env.API_PORT}`);
+app.listen(process.env.API_PORT || 4000, () => {
+	console.log(`Servidor corriendo en ${process.env.API_PORT || 4000}`);
 });
