ea74d4ed729ed1445f4ce5cecee5939276b897b2
Miguel Alejandro Salgado Ramírez <alejandrosram@outlook.com>
2022-08-08 10:37:32 -0500

    Update public folder routing

--- index.js
+++ index.js
@@ -17,6 +17,10 @@ app.use(express.json());
 app.use("/api/auth", require("./routes/auth.route"));
 app.use("/api/events", require("./routes/event.route"));
 
+app.get("*", (req = express.request, res = express.response) => {
+	res.sendFile(`${__dirname}/public/index.html`);
+});
+
 app.listen(process.env.PORT || 4000, () => {
 	console.log(`Servidor corriendo en ${process.env.PORT || 4000}`);
 });
